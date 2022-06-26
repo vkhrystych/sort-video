ab26d0667728d4f979a1e5f5bc297f19fcab4f95
Vladyslav Khrystych <vladyslavkhrystych@gmail.com>
2022-06-26 14:19:07 +0300

    Added subscribe with email form

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,89 @@
-new Glide(".glide", {
-  rewind: true,
-}).mount();
+const airtable_api_key = "keyfU8G0B8LTbCIK1";
+const dbId = "appq2HC7SSA3rasiU";
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+const base = new Airtable({ apiKey: airtable_api_key }).base(dbId);
+
+const tableId = "tbllcKAtFuCUhkoF0";
+const tableEmailFieldId = "fldgKzvRxexpzn3FH";
+
+const headers = new Headers({
+  Authorization: `Bearer ${airtable_api_key}`,
+});
+
+const addEmailToAirtable = (emailValue) => {
+  base("Emails").create(
+    [
+      {
+        fields: {
+          email: emailValue,
+        },
+      },
+    ],
+    function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record) {
+        alert("Thanks for subscribing!");
+
+        const subscribeInput = document.querySelector("#subscribe-email");
+
+        subscribeInput.value = "";
+      });
+    }
+  );
+};
+
+const validateEmail = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const onSubscribeBtnClick = (e) => {
+  const subscribeInput = document.querySelector("#subscribe-email");
+  const email = subscribeInput.value;
+
+  const isEmailValid = validateEmail(email);
+
+  if (!email || !isEmailValid) {
+    return;
+  }
+
+  if (email) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    addEmailToAirtable(email);
+  }
+};
+
+const addSmoothScollToAnchorElements = () => {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
   });
-});
+};
+
+const addOnClickHandlers = () => {
+  document.querySelector("#subscribe-btn").onclick = onSubscribeBtnClick;
+};
+
+const initSlider = () => {
+  new Glide(".glide", {
+    rewind: true,
+  }).mount();
+};
+
+const initApp = () => {
+  initSlider();
+  addOnClickHandlers();
+  addSmoothScollToAnchorElements();
+};
+
+initApp();
